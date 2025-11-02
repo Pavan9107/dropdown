e697143e19463d575ce16df88c8ae9534201115c
Prajwal23S <yedprajwal@gmail.com>
2025-11-02 14:29:21 +0530

    initial commit

---
+++ drop_down_select.py
@@ -0,0 +1,40 @@
+import time
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://www.amazon.in/")
+dropdown_element = driver.find_element(By.ID, "searchDropdownBox")
+dropdown = Select(dropdown_element)
+dropdown.select_by_visible_text("Books")
+time.sleep(3)
+
+selected_option = dropdown.first_selected_option.text
+assert selected_option == "Books"
+
+driver.find_element(By.ID, "twotabsearchtextbox").send_keys("python")
+
+wait = WebDriverWait(driver, 10)
+wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div.s-suggestion")))
+
+suggestions = driver.find_elements(By.CSS_SELECTOR, "div.s-suggestion")
+for suggestion in suggestions:
+    if suggestion.get_attribute("aria-label") == "python book":
+        suggestion.click()
+        time.sleep(5)
+        break
+
+
+
+
+
+
+
+
